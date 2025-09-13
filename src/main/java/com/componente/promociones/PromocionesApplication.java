64c0b481882b9416d81f89d602894e4650c8919f
Steven Rivera <steven.melendez001@gmail.com>
2025-09-12 22:40:01 -0600

    Configuracion CORS agregada para comunicacion Backend -> Frontend

--- src/main/java/com/componente/promociones/PromocionesApplication.java
+++ src/main/java/com/componente/promociones/PromocionesApplication.java
@@ -16,17 +16,31 @@ public class PromocionesApplication {
 		SpringApplication.run(PromocionesApplication.class, args);
 	}
 
-	//para comunicacion con el frontend
 	@Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		// Agregar http://localhost:3000 para Next.js
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList(
+				"Origin",
+				"Access-Control-Allow-Origin",
+				"Content-Type",
+				"Accept",
+				"Authorization",
+				"Origin, Accept",
+				"X-Requested-With",
+				"Access-Control-Request-Method",
+				"Access-Control-Request-Headers"
+		));
+		corsConfiguration.setExposedHeaders(Arrays.asList(
+				"Origin",
+				"Content-Type",
+				"Accept",
+				"Authorization",
+				"Access-Control-Allow-Origin",
+				"Access-Control-Allow-Credentials"
+		));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
